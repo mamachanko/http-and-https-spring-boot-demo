5039dcdc7d77292d8ce088400644a90416f5a89d
Max Brauer <mbrauer@vmware.com>
2021-09-09 16:47:08 +0200

    Expose both HTTP and HTTPS from the same app

---
+++ src/main/java/com/vmware/tanzu/app/sso/demo/http/https/Application.java
@@ -0,0 +1,16 @@
+package com.vmware.tanzu.app.sso.demo.http.https;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmware/tanzu/app/sso/demo/http/https/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.tanzu.app.sso.demo.http.https;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
